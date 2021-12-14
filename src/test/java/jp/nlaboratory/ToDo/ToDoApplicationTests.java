b98bfc4d5352066393096d1322b28fb2e87d727e
n.nakanishi <n.nakanishi@sync-web.jp>
2021-12-14 21:53:50 +0900

    .gitignoreを反映

---
+++ src/test/java/jp/nlaboratory/ToDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.nlaboratory.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
